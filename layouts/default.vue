1ecbc862d6a9f3340a55b5091c2998eb3353fa3f
Kyo Yamada <hysteric.madam@gmail.com>
2020-09-04 22:10:44 +0900

    add: pug

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,42 +1,28 @@
-<template>
-  <div>
-    <nav
-      class="navbar header has-shadow is-primary"
-      role="navigation"
-      aria-label="main navigation"
-    >
-      <div class="navbar-brand">
-        <a class="navbar-item" href="/">
-          <img src="~assets/buefy.png" alt="Buefy" height="28" />
-        </a>
+<template lang="pug">
+  div
+    nav.navbar.header.has-shadow.is-primary(role='navigation' aria-label='main navigation')
+      .navbar-brand
+        a.navbar-item(href='/')
+          img(src='~assets/buefy.png' alt='Buefy' height='28')
+        .navbar-burger
+          span
+            span
+              span.
 
-        <div class="navbar-burger">
-          <span />
-          <span />
-          <span />
-        </div>
-      </div>
-    </nav>
+    section.main-content.columns
+      aside.column.is-2.section
+        p.menu-label.is-hidden-touch.
 
-    <section class="main-content columns">
-      <aside class="column is-2 section">
-        <p class="menu-label is-hidden-touch">
           General
-        </p>
-        <ul class="menu-list">
-          <li v-for="(item, key) of items" :key="key">
-            <nuxt-link :to="item.to" exact-active-class="is-active">
-              <b-icon :icon="item.icon" /> {{ item.title }}
-            </nuxt-link>
-          </li>
-        </ul>
-      </aside>
 
-      <div class="container column is-10">
-        <nuxt />
-      </div>
-    </section>
-  </div>
+        ul.menu-list
+          li(v-for='(item, key) of items' :key='key')
+            nuxt-link(:to='item.to' exact-active-class='is-active')
+              b-icon(:icon='item.icon').
+                {{ item.title }}
+
+      .container.column.is-10
+        nuxt.
 </template>
 
 <script>
